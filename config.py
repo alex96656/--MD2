c0f6e4c1ba16f137d470d6509e9305a2ec669d2d
〖ᴹᴿ•ᴀʟᴇ᥊᭄𓋆 ⁰⁰³ <godwinuzochi51@gmail.com>
2026-05-16 01:34:14 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+class Config:
+    BOT_NAME = "ᴹᴿ•ᴀʟᴇ᥊-MD"
+    OWNER_NAME = "〖ᴹᴿ•ᴀʟᴇ᥊᭄𓋆 ⁰⁰³"
+    OWNER_NUMBER = "2347032527540"
+    TELEGRAM_OWNER = "@mr_alex_dem"
+    PREFIX = "."
+    # Use any anime wallpaper link here
+    ANIME_IMG = "https://wallpapercave.com/wp/wp8219463.jpg" 
+    
+    DATABASE_FILE = "database.json"
+    WEB_HOST = "0.0.0.0"
+    WEB_PORT = 8000
