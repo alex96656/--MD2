e9a7aa361725d314ee6d090560a5604afb15c0d0
〖ᴹᴿ•ᴀʟᴇ᥊᭄𓋆 ⁰⁰³ <godwinuzochi51@gmail.com>
2026-05-17 00:42:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,28 @@ from flask import Flask, render_template, request, jsonify
 from config import Config
 import random
 import string
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', bot_name=Config.BOT_NAME, telegram=Config.TELEGRAM_OWNER, bg_img=Config.ANIME_IMG)
+    return render_template('index.html',
+                           bot_name=Config.BOT_NAME,
+                           telegram=Config.TELEGRAM_OWNER,
+                           bg_img=Config.ANIME_IMG)
 
 @app.route('/generate-code', methods=['POST'])
 def generate_code():
     phone = request.json.get('number')
-    if not phone: return jsonify({"success": False})
-    
-    # Random pairing code generation
+    if not phone:
+        return jsonify({"success": False})
+
     code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) + "-" + \
            ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
-    
+
     return jsonify({"success": True, "code": code})
 
-if __name__ == '__main__':
-    app.run(host=Config.WEB_HOST, port=Config.WEB_PORT)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
